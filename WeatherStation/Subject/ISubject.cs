9c8ec920635e3b0d81a52cbd0544940a2cd0e9cc
Joseph Rogina <Joseph.Rogina@waystar.com>
2021-04-20 11:02:50 -0700

    Decorator Pattern started.

---
+++ WeatherStation/Subject/ISubject.cs
@@ -0,0 +1,11 @@
+﻿using WeatherStation.Observers;
+
+namespace WeatherStation.Subject
+{
+    public interface ISubject
+    {
+        public void RegisterObserver(IObserver observer);
+        public void RemoveObserver(IObserver observer);
+        public void NotifyObservers();
+    }
+}
